38946cf647f080b11d6f03163688b589b6c5941c
Samman <sammanbhetwal2025@gmail.com>
2023-04-02 20:02:52 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+//App.js
 import './App.css';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const commands = [
+ {
+    command: 'reset',
+    callback: ({ resetTranscript }) => {
+  resetTranscript();
+  }
+},
+
+{
+  command: 'clear',
+  callback: ({resetTranscript}) => {
+    resetTranscript();
+  }
+},
+{
+  command: 'open *',//* will capture the "google.com"
+  callback: (site) => {window.open('http://'+site)}
+},
+{
+  command: 'increase text size',
+  callback: () => {document.getElementById('content').style.fontSize = '22px'}
+},
+{
+  command: 'decrease text size',
+  callback: () => {document.getElementById('content').style.fontSize = '16px'}
+},
+{
+  command: 'change text color to *',
+  callback: (color) => {document.getElementById('content').style.color =  color}
 }
 
+
+  ]
+ SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+
+  const {
+    transcript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition({commands});//commands should be passed as 
+  //object and not as props
+
+  
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+
+
+return (
+ <div className="container">
+  <div className="nav">
+    <h2>Please speak something</h2>
+  </div>
+  <div id='content'>
+{transcript}
+  </div>
+ </div>
+);
+};
 export default App;
